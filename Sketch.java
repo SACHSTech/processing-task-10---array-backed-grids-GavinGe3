9f1f885056a7f189545d52e8afff19d2168d4693
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-05-19 09:34:36 -0400

    Hello

--- Sketch.java
+++ Sketch.java
@@ -2,6 +2,7 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
+
   float fltCellWidth = 20;
   float fltCellHeight = 20;
   float fltCellMargin = 5;
@@ -11,6 +12,9 @@ public class Sketch extends PApplet {
 
   float fltScreenWidth = fltCellWidth * intColumnCount + (fltCellMargin * (intColumnCount+1));
   float fltScreenHeight = fltCellHeight* intRowCount + (fltCellMargin * (intRowCount+1));
+
+
+  int [][] intGrid = new int[intRowCount][intColumnCount];
 
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -25,24 +29,33 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+    intGrid[1][5] = 1;
     background(0, 0, 0);
-
     for(int column = 0; column < intColumnCount; column++){
-      rect(column * fltCellWidth + fltCellMargin, 0, fltCellWidth, fltCellHeight);
+      for (int row = 0; row < intRowCount; row++){
+        if (intGrid[column][row] == 1){
+          fill(0,128,0);
+        }
+        else{
+          fill(255,255,255);
+        }
+        rect(column * fltCellWidth + fltCellMargin, row * fltCellHeight, fltCellWidth, fltCellHeight);
+      }
+    }
+
+   
     }
 
 
-
-
-
-
-
-  }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    intGrid[1][5] = 1;
+    fill(255,255,255);
+    
+
 
 	  
   }
